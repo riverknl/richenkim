e1e3676a542a52d107a7972e73cd08025f49ce1c
Richard Verkerk <riverk@gmail.com>
2020-06-18 17:31:43 +0200

    Update programma.blade.php

--- resources/views/pages/programma.blade.php
+++ resources/views/pages/programma.blade.php
@@ -8,13 +8,48 @@
                     <h1 style="text-align:center;"><strong>Programma</strong></h1>
                 </div>
                 <hr>
+
                 <div class="card">
                     <div class="card-body">
-                        15.00 - 16.00 Ceremonie<br/>
-                        16.00 - 18.00 Receptie<br/>
-                        @if($guest->special_guest)
-                            18.00 - 23.00  Diner
-                        @endif
+                        <div class="container text-center">
+                            <div class="row m-5 p-5" style="border-bottom: 1px solid #f9f9f9;">
+                                <div class="col-md-4 offset-2 text-center">
+                                    <h2 style="font-family: 'Amatic SC'; font-size: 3em;">14.45 uur</h2>
+                                    <h3>Aanvang</h3>
+                                </div>
+                                <div class="col-md-4">
+                                    <i class="fa fa-clock" style="font-size:5em"></i>
+                                </div>
+                            </div>
+                            <div class="row  p-5" style="border-bottom: 1px solid #f9f9f9;">
+                                <div class="col-md-4 offset-2 text-center"><i
+                                        class="fa fa-frog" style="font-size:5em"></i></div>
+                                <div class="col-md-4">
+                                    <h2 style="font-family: 'Amatic SC'; font-size: 3em;">15.00 uur</h2>
+                                    <h3>Trouw Ceremonie</h3>
+                                </div>
+                            </div>
+                            <div class="row p-5" style="border-bottom: 1px solid #f9f9f9;">
+                                <div class="col-md-4 offset-2 text-center">
+                                    <h2 style="font-family: 'Amatic SC'; font-size: 3em;">16.00 uur</h2>
+                                    <h3>Receptie</h3>
+                                </div>
+                                <div class="col-md-4">
+                                    <i class="fa fa-glass-martini-alt" style="font-size:5em"></i>
+                                </div>
+                            </div>
+                            @if($guest->special_guest)
+                                <div class="row  p-5" style="border-bottom: 1px solid #f9f9f9;">
+                                    <div class="col-md-4 offset-2 text-center"><span class="align-middle"><i
+                                                class="fa fa-utensils" style="font-size:5em"></i></span></div>
+                                    <div class="col-md-4">
+                                        <h2 style="font-family: 'Amatic SC'; font-size: 3em;">18.00 uur</h2>
+                                        <h3>Diner</h3>
+                                    </div>
+                                </div>
+                            @endif
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -28,13 +63,36 @@
                 <hr>
                 <div class="card">
                     <div class="card-body">
-                        <p>
-                            15.00 - 16.00 Ceremonie<br/>
-                            16.00 - 18.00 Receptie<br/>
-                        </p>
+                        <div class="container text-center">
+                            <div class="row m-5 p-5" style="border-bottom: 1px solid #f9f9f9;">
+                                <div class="col-md-4 offset-2 text-center">
+                                    <h2 style="font-family: 'Amatic SC'; font-size: 3em;">14.45 uur</h2>
+                                    <h3>Aanvang</h3>
+                                </div>
+                                <div class="col-md-4">
+                                    <i class="fa fa-clock" style="font-size:5em"></i>
+                                </div>
+                            </div>
+                            <div class="row  p-5" style="border-bottom: 1px solid #f9f9f9;">
+                                <div class="col-md-4 offset-2 text-center"><i
+                                        class="fa fa-frog" style="font-size:5em"></i></div>
+                                <div class="col-md-4">
+                                    <h2 style="font-family: 'Amatic SC'; font-size: 3em;">15.00 uur</h2>
+                                    <h3>Trouw Ceremonie</h3>
+                                </div>
+                            </div>
+                            <div class="row p-5" style="border-bottom: 1px solid #f9f9f9;">
+                                <div class="col-md-4 offset-2 text-center">
+                                    <h2 style="font-family: 'Amatic SC'; font-size: 3em;">16.00 uur</h2>
+                                    <h3>Receptie</h3>
+                                </div>
+                                <div class="col-md-4">
+                                    <i class="fa fa-glass-martini-alt" style="font-size:5em"></i>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
     @endif
 @endsection
