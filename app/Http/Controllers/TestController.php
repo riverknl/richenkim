66d91ea655c48fe68301fa305387dda6786da2a9
Richard Verkerk <r.verkerk@yazoom.nl>
2020-12-11 16:54:19 +0100

    clear

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,26 +15,26 @@ class TestController extends Controller
 //        $hashed = Hash::make($password);
 //        echo $password . '<br />' . $hashed;
 
-        $tokens = DB::table('guests')->select(DB::raw('token'))->groupBy('token')->get();
-        foreach($tokens->sortBy('token') as $token)
-        {
-            $guests = Guest::where('token', $token->token)->get();
-            echo '<strong>Token:</strong> ' . $token->token . '<br/>';
-            $special_guest = $guests->pluck('special_guest')->toArray();
-            if(in_array(1,$special_guest))
-            {
-                echo '<strong>Dag gast:</strong> JA <br />';
-            } else
-            {
-                echo '<strong>Dag gast:</strong> NEE <br />';
-
-            }
-            foreach($guests as $guest)
-            {
-                echo '<strong>Naam:</strong> ' . $guest->fullName . '<br />';
-            }
-            echo '---- <br />';
-        }
+//        $tokens = DB::table('guests')->select(DB::raw('token'))->groupBy('token')->get();
+//        foreach($tokens->sortBy('token') as $token)
+//        {
+//            $guests = Guest::where('token', $token->token)->get();
+//            echo '<strong>Token:</strong> ' . $token->token . '<br/>';
+//            $special_guest = $guests->pluck('special_guest')->toArray();
+//            if(in_array(1,$special_guest))
+//            {
+//                echo '<strong>Dag gast:</strong> JA <br />';
+//            } else
+//            {
+//                echo '<strong>Dag gast:</strong> NEE <br />';
+//
+//            }
+//            foreach($guests as $guest)
+//            {
+//                echo '<strong>Naam:</strong> ' . $guest->fullName . '<br />';
+//            }
+//            echo '---- <br />';
+//        }
 
     }
 }
