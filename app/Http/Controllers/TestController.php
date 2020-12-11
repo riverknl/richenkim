0082dee4d1a38a6d6dd601bd0c2499803dcdf7de
Richard Verkerk <r.verkerk@yazoom.nl>
2020-12-11 16:36:43 +0100

    gasten lijst

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use App\Guest;
 
 class TestController extends Controller
 {
@@ -12,5 +13,20 @@ class TestController extends Controller
 //        $password = '123LKJMN0';
 //        $hashed = Hash::make($password);
 //        echo $password . '<br />' . $hashed;
+
+        $tokens = Guest::all();
+
+        $token = '';
+        foreach($tokens->sortBy('token') as $token)
+        {
+            $guests = Guest::where('token', $token->token)->get();
+            echo '<strong>Token:</strong> ' . $token->token . '<br/>';
+            foreach($guests as $guest)
+            {
+                echo '<strong>Naam:</strong> ' . $guest->fullName . '<br />';
+            }
+            echo '---- <br />';
+        }
+
     }
 }
